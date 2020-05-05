be3d8c1920eed70d6e9b52fa8ca3f625d1609673
Brayan Florez <bflorez@outlook.com>
2020-05-06 00:49:47 +0200

    Updates

---
+++ precache-manifest.438f7b36aaebb6103151aee8cc57d067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ed4ffdffc8e3de328e9fd3f58aa466",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a2d459cf156fdcfdc3cf",
+    "url": "/monsters-rolodex/static/css/main.8dbd0661.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d459cf156fdcfdc3cf",
+    "url": "/monsters-rolodex/static/js/main.d1e6caf3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
